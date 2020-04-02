13c47adac1e17454bf06c2b5a215ed6436c42ed3
Vahan <vpiroyan@gmail.com>
2020-04-03 00:28:23 +0400

    first commit

---
+++ script_old.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+
+var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+var string = 'Hello';
+let nnnnn;
+
+console.log (4/0);
+console.log (string);
+console.log (nnnnn);
+
+let person = {
+name: "John",
+age: 25,
+isMarried:false
+};
+console.log (person['name']);
+
+let arr = ['plum.png','orange.jpg','em.jpg'];
+
+console.log (arr[2]);
+
+//alert ('Hello World!');
+
+/*let answer=confirm ("Are you here?");
+
+console.log (answer);*/
+
+/*let answer=prompt ("Are you 18?","Yes");
+
+console.log (answer);*/
+
+/*console.log ("arr"+"-object");
+console.log (4++"-object");
+
+let answer=+ prompt ("Are you 18?","Yes");
+
+console.log (typeof(answer));*/
+
+let incr=10;
+let decr=10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+let isCh=true,
+    isCl=false;
+    console.log(isCh || isCl);
+
+a=5;
+console.log(a);
